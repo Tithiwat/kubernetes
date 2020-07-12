90c728995df171ad35cc728518ec93ee6346d870
Tithiwat <tithiwattong@gmail.com>
2020-07-12 21:01:33 +0700

    add create cluster

--- scripts/gcloud_nat/create_cluster.sh
+++ scripts/gcloud_nat/create_cluster.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
+# Instruction from here
+# https://cloud.google.com/solutions/creating-kubernetes-engine-private-clusters-with-net-proxies
+
 export INSTANCE_NAME=ubuntu
 export REGION=us-central1
 export ZONE=us-central1-c
+# export REGION=asia-east1
+# export ZONE=asia-east1-b
 
 gcloud config set compute/region $REGION
 gcloud config set compute/zone $ZONE
 export CLIENT_IP=`gcloud compute instances describe $INSTANCE_NAME --format="value(networkInterfaces[0].networkIP)"`
 
-export CLUSTER_NAME=private-rook
+export CLUSTER_NAME=rook
+export NETWORK_NAME=k8s-proxy
 export CLUSTER_SUBNETWORK=subnet-cluster
+export MASTER_CIDR=172.16.0.64/28
+# --create-subnetwork name=$CLUSTER_SUBNETWORK \
 
-# gcloud container clusters create private-cluster \
-#     --create-subnetwork name=private-cluster \
-#     --enable-ip-alias \
-#     --enable-private-nodes \
-#     --master-ipv4-cidr 172.16.0.0/28 \
-#     --enable-master-authorized-networks \
-#     --master-authorized-networks $CLIENT_IP/32
-#     --no-enable-basic-auth \
-#     --no-issue-client-certificate
-
-
-gcloud container clusters create $CLUSTER_NAME \
-    --network=k8s-proxy \
+gcloud container clusters create $CLUSTER_NAME  \
+    --master-ipv4-cidr=$MASTER_CIDR \
+    --network $NETWORK_NAME \
     --subnetwork=$CLUSTER_SUBNETWORK \
     --enable-ip-alias \
     --enable-private-nodes \
-    --master-ipv4-cidr 172.16.0.0/28 \
-    --enable-master-authorized-networks \
+    --enable-private-endpoint \
     --master-authorized-networks $CLIENT_IP/32 \
-    --no-enable-basic-auth \
-    --no-issue-client-certificate
+    --enable-master-authorized-networks \
+    --num-nodes 3
 
 # bash -i
