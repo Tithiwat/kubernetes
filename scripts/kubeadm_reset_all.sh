461e7c66014761186aceedaf587172e702dac2f5
Tithiwat <tithiwattong@gmail.com>
2020-08-10 17:30:33 +0700

    update

---
+++ scripts/kubeadm_reset_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+echo "Please input node name: $0 [node name]"
+else
+kubectl drain $1 --delete-local-data --force --ignore-daemonsets
+kubectl delete node $1
+
+kubeadm reset
+fi
